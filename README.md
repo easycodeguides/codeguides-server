d7d0bdcd6381deea313df50b62985dc619d055a4
zarko <zivojnovic@gmail.com>
2020-01-22 22:53:34 +0100

    health check route changed

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ run `node index.js`
 
 ### Health check
 
-go to `localhost:3000/health-check`
+go to `localhost:3000/healthcheck`
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/health-check', (req, res) => res.send('I\'m alive!'))
+app.get('/healthcheck', (req, res) => res.send('<h3>I\'m alive!</h3>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
